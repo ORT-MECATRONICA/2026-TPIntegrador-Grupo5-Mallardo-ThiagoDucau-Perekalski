f5d2c594e8dee640d207e7a5b0233e99c60c7bb9
MarcoMallardo <49379361@est.ort.edu.ar>
2026-08-28 09:38:02 -0300

    Probar ambos codigos para display

--- Modulos/BME280/BME280.ino
+++ Modulos/BME280/BME280.ino
@@ -1,22 +1,23 @@
+// Grupo 5: Marco Mallardo, Thiago Ducau, Ramiro Perekalski
+
 #include <Arduino.h>
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+#include <Adafruit_BMP280.h>
 
-Adafruit_BME280 bme;
+Adafruit_BMP280 bmp;
 
 void setup() {
-  Serial.begin(115200);  
-  bool status = bme.begin(0x76);
+  Serial.begin(115200);
+  bool status = bmp.begin(0x76);
   if (!status) {
-    Serial.println("Error al conectar bme");
+    Serial.println("Error al conectar bmp");
     while (1); //Frenar el programa
- }
+  }
 }
 
 void loop() {
-  Serial.print("Temp:"); Serial.println(bme.readTemperature());
-  Serial.print("Humidity:"); Serial.println(bme.readHumidity());
+  Serial.print("Temperatura: "); Serial.println(bmp.readTemperature());
+  Serial.print("Pressure (hPa):"); Serial.println(bmp.readPressure() / 100.0F);
   delay(5000);
 }
