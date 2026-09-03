307d7dee26e281ca54be05608cd0029f8b6a3443
MarcoMallardo <49379361@est.ort.edu.ar>
2026-09-03 10:11:54 -0300

    Actualiza codigo de prueba para HLK-LD2410 usando lectura por UART

--- Modulos/HLK-LD2410/HLK-LD2410.ino
+++ Modulos/HLK-LD2410/HLK-LD2410.ino
@@ -2,22 +2,74 @@
 
 #include <Arduino.h>
 
-// El sensor HLK-LD2410 tiene un pin OUT que se pone en ALTO cuando detecta presencia
-const int radarOutPin = 32; 
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(radarOutPin, INPUT);
-}
-
-void loop() {
-  int presencia = digitalRead(radarOutPin);
-  
-  if (presencia == HIGH) {
-    Serial.println("Presencia detectada!");
-  } else {
-    Serial.println("Sin presencia");
-  }
-  
+#define MONITOR_SERIAL Serial
+#define RADAR_SERIAL Serial1
+#define RADAR_RX_PIN 16
+#define RADAR_TX_PIN 17
+ 
+#include <ld2410.h>
+ 
+ld2410 radar;
+ 
+uint32_t lastReading = 0;
+ 
+void setup(void)
+{
+  MONITOR_SERIAL.begin(115200); // Feedback over Serial Monitor
+  // radar.debug(MONITOR_SERIAL); Uncomment to show debug information from the library on the Serial Monitor. By default this does not show sensor reads as they are very frequent.
+ 
+  RADAR_SERIAL.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN); // UART for monitoring the radar
   delay(500);
+  MONITOR_SERIAL.print(F("\nConnect LD2410 radar TX to GPIO:"));
+  MONITOR_SERIAL.println(RADAR_RX_PIN);
+  MONITOR_SERIAL.print(F("Connect LD2410 radar RX to GPIO:"));
+  MONITOR_SERIAL.println(RADAR_TX_PIN);
+  MONITOR_SERIAL.print(F("LD2410 radar sensor initialising: "));
+ 
+  if (radar.begin(RADAR_SERIAL))
+  {
+    MONITOR_SERIAL.println(F("OK"));
+    MONITOR_SERIAL.print(F("LD2410 firmware version: "));
+    MONITOR_SERIAL.print(radar.firmware_major_version);
+    MONITOR_SERIAL.print('.');
+    MONITOR_SERIAL.print(radar.firmware_minor_version);
+    MONITOR_SERIAL.print('.');
+    MONITOR_SERIAL.println(radar.firmware_bugfix_version, HEX);
+  }
+  else
+  {
+    MONITOR_SERIAL.println(F("not connected"));
+  }
+}
+ 
+void loop()
+{
+  radar.read();
+  if (radar.isConnected() && millis() - lastReading > 1000) // Report every 1000ms
+  {
+    lastReading = millis();
+    if (radar.presenceDetected())
+    {
+      if (radar.stationaryTargetDetected())
+      {
+        Serial.print(F("Stationary target: "));
+        Serial.print(radar.stationaryTargetDistance());
+        Serial.print(F("cm energy:"));
+        Serial.print(radar.stationaryTargetEnergy());
+        Serial.print(' ');
+      }
+      if (radar.movingTargetDetected())
+      {
+        Serial.print(F("Moving target: "));
+        Serial.print(radar.movingTargetDistance());
+        Serial.print(F("cm energy:"));
+        Serial.print(radar.movingTargetEnergy());
+      }
+      Serial.println();
+    }
+    else
+    {
+      Serial.println(F("No target"));
+    }
+  }
 }
