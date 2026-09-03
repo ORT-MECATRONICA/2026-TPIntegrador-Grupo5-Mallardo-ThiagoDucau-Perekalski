4beff9f1a8ca4d5414f1b524448d5c6f01281721
MarcoMallardo <49379361@est.ort.edu.ar>
2026-09-03 16:29:57 -0300

    Agrega encendido de LED al superar el umbral en sensor MQ-2

--- Modulos/MQ-2/MQ-2.ino
+++ Modulos/MQ-2/MQ-2.ino
@@ -2,15 +2,27 @@
 
 #include <Arduino.h>
 
-const int mq2Pin = 35; // Pin analogico simulando el MQ-2 con un potenciometro
+const int mq2Pin = 35; // Pin analogico para el MQ-2 (o potenciometro)
+const int ledPin = 2;  // Pin para el LED indicador
 
 void setup() {
   Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
   int sensorValue = analogRead(mq2Pin);
-  Serial.print("Valor MQ-2 (simulado): ");
+  
+  Serial.print("Valor MQ-2: ");
   Serial.println(sensorValue);
+  
+  if (sensorValue > 250) {
+    digitalWrite(ledPin, HIGH); // Prende el LED
+    Serial.println("¡Alerta! Nivel superado.");
+  } else {
+    digitalWrite(ledPin, LOW);  // Apaga el LED
+  }
+  
   delay(1000);
 }
