3fcf01aba9e29cf8e6ceda8c8b88e3384500d28e
MarcoMallardo <49379361@est.ort.edu.ar>
2026-09-03 17:23:44 -0300

    Limpia comentarios en el código del LDR

--- Modulos/LDR/LDR.ino
+++ Modulos/LDR/LDR.ino
@@ -5,9 +5,6 @@
 const int ldrPin = 34; // Pin analogico conectado entre la R de 10k y el LDR
 const int ledPin = 2;  // Pin para el LED indicador
 
-// Umbral de oscuridad. Dependiendo de la placa (ESP32 de 12 bits o Arduino de 10 bits), 
-// este valor puede variar. Ajustar segun iluminacion ambiente.
-// Con la conexion 5V - 10k - pin - LDR - GND:
 // A mayor oscuridad, mayor resistencia del LDR, por lo que el pin lee un voltaje mayor.
 const int umbralOscuridad = 2000; 
 
